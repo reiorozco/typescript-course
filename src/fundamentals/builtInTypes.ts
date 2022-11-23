4120b97dc432121c0ea07a27830623613ce8d8b6
Rei Orozco <rfoc15@gmail.com>
2022-11-23 12:49:23 -0500

    5- Arrays, Tuples, Enums

--- src/fundamentals/builtInTypes.ts
+++ src/fundamentals/builtInTypes.ts
@@ -6,3 +6,29 @@ let level;
 function render(document: any) {
   console.log(document);
 }
+
+// # Arrays
+let numbersAndString: (number | string)[] = [1, 2, "3"];
+let numbers: number[] = [1, 2, 3];
+
+// # Tuples
+let user: [number, string] = [1, "Rei"];
+
+// # Enums
+const small = 1;
+const medium = 2;
+const large = 3;
+
+const enum Size {
+  Small,
+  Medium,
+  Large,
+}
+
+enum OtherSizes {
+  Small = "S",
+  Medium = "M",
+  Large = "L",
+}
+
+let mySize: OtherSizes = OtherSizes.Medium;
