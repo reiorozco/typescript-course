108004c5a7e6a3c70a7f66d9288b709f656e1f43
Rei Orozco <rfoc15@gmail.com>
2022-11-24 10:20:37 -0500

    8- Getters and Setters

--- src/4-OOP/classes.ts
+++ src/4-OOP/classes.ts
@@ -18,14 +18,22 @@ class Account {
 
   // private calculateTax() {}
 
-  getBalance() {
+  get balance() {
     return this._balance;
+  }
+
+  set balance(value: number) {
+    if (value < 0) throw new Error("Invalid value");
+
+    this._balance = value;
   }
 }
 
 export let account = new Account(1, "Rei", 2500);
 account.deposit(500);
+account.owner = "Fabio";
+account.balance = 2000; // set Balance(Getters and Setters)
 
-console.log("getBalance", account.getBalance());
+console.log("get Balance", account.balance);
 console.log("typeof", typeof account);
 console.log("instanceof", account instanceof Account);
