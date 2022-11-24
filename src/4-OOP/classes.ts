4e5bb78e0bd61e746655fe28ba4252905071d192
Rei Orozco <rfoc15@gmail.com>
2022-11-24 09:48:56 -0500

    6- Access Control Keywords

--- src/4-OOP/classes.ts
+++ src/4-OOP/classes.ts
@@ -1,13 +1,13 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
@@ -15,12 +15,19 @@ class Account {
       throw new Error("Invalid amount");
     }
 
-    this.balance += amount;
+    // Record a transaction
+    this._balance += amount;
+  }
+
+  private calculateTax() {}
+
+  getBalance() {
+    return this._balance;
   }
 }
 
 export let account = new Account(1, "Rei", 2500);
 account.deposit(500);
-console.log(account.balance);
+console.log("getBalance", account.getBalance());
 console.log("typeof", typeof account);
 console.log("instanceof", account instanceof Account);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { age } from "./hello-world";
+// import { age } from "./hello-world";
 import { account } from "./4-OOP/classes";
 
-console.log(age);
+// console.log(age);
 
 console.log(account);
