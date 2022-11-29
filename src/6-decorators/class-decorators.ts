f21ecc6fd309a3094f53644ba13dc8a2c85f006e
Rei Orozco <rfoc15@gmail.com>
2022-11-29 07:49:25 -0500

    4- Parameterized Decorators

--- src/6-decorators/class-decorators.ts
+++ src/6-decorators/class-decorators.ts
@@ -1,12 +1,20 @@
-function Component(constructor: Function) {
-  console.log("Component decorator called.");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Inserting the component in the DOM.");
+// # Decorator Factory
+type ComponentOptions = {
+  selector: string;
+};
+
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called.");
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the DOM.");
+    };
+    constructor.prototype.options = options;
   };
 }
 
-@Component
+@Component({ selector: "#id-componentToSelect" })
 class ProfileComponent {
   constructor(public name: string) {}
 }
