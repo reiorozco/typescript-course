95443f15cbd0d83d86adb301dbd3c56ef05ddf49
reiorozco <rfoc15@gmail.com>
2022-12-07 14:20:35 -0500

    8 Integration-with-JS

--- index.ts
+++ index.ts
@@ -2,6 +2,15 @@ import { ProfileComponent } from "./7 Decorators/class-decorators";
 import { Person } from "./7 Decorators/method-decorators";
 import { User } from "./7 Decorators/property-decorators";
 
+import { calculateTax } from "./8 Integration-with-JS/tax";
+
+import * as _ from "lodash";
+
 console.log(ProfileComponent);
 console.log(Person);
 console.log(User);
+
+let tax = calculateTax(100);
+console.log(tax);
+
+_.clone([1, 2, 3]);
